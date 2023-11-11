1fa17cade615daecb22dea41cbc9edc87b1934cf
MadhuKasi123 <madhuroyal070@gmail.com>
2023-11-11 09:10:54 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,7 @@
+from csk.views import *
+from django.urls import path
+app_name='madhu'
+urlpatterns=[
+
+    path('msd',msd,name='msd')
+]
---
+++ mi/urls.py
@@ -0,0 +1,7 @@
+from mi.views import *
+from django.urls import path
+app_name='madhu'
+urlpatterns=[
+
+    path('rohit',rohit,name='rohit')
+]
